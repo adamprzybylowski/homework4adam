94620975477f0f11d46810f97632a727b21f4be9
Adam Przybyłowski <adam.przybylowski@wp.pl>
2020-01-06 20:22:16 +0100

    2 basic tests passed

--- src/main/java/com/infoshareacademy/homeworks/GradeService.java
+++ src/main/java/com/infoshareacademy/homeworks/GradeService.java
@@ -1,10 +1,15 @@
 package com.infoshareacademy.homeworks;
 
+import java.util.*;
+
 public class GradeService {
 
-  public String[][] calculateAverage(String[][] data) {
+    public String[][] calculateAverage(String[][] data) {
+        return (data == null || data.length == 0) ? new String[][]{,} :
+                Arrays.stream(data)
+                        .toArray(String[][]::new);
 
-    return null;
-
-  }
+    }
 }
+
+
